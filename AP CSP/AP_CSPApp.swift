6c68361ae4c429241252375a1155413d0ea827c0
Abe Park <abepark013@gmail.com>
2023-04-20 07:57:07 -0400

    Initial Commit

---
+++ AP CSP/AP_CSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_CSPApp.swift
+//  AP CSP
+//
+//  Created by Abe on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AP_CSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
